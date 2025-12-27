d1c0d4fab50179e09e2793e5fe196d31cf01f047
RobertoG <3008431000101@ingenieria.usac.edu.gt>
2025-12-26 22:18:11 -0600

    feat(CI/CD): Kubernets deploy

--- services/frontend/src/components/Layout.tsx
+++ services/frontend/src/components/Layout.tsx
@@ -14,7 +14,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
       <nav className="navbar">
         <div className="navbar-content">
           <div className="navbar-brand">
-            QuetzalShip
+            QuetzalShip - KUBERNETS
           </div>
           
           <div className="navbar-links">
